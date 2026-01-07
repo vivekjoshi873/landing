de0748df67156070ba5917e6c9c5ee5ffca776d9
Vivek Joshi <vj69376@gmail.com>
2026-01-07 17:38:31 +0530

    inital commit

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ if (benefitsGrid && siteContent.benefits) {
           ${p.features
             .map(
               (f) =>
-                `<li class="flex ml-4 text-[15px]  items-center text-sm text-gray-600"><svg class="w-4 h-4 text-primary mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"/></svg>${f}</li>`
+                `<li class="flex ml-4 text-[15px]  items-center text-sm text-gray-600 font-semibold"><svg class="w-4 h-4 text-primary mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"/></svg>${f}</li>`
             )
             .join("")}
         </ul>
